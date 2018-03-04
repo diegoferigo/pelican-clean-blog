cfec6e103db4dc8840d94570a9f3611629a59c93
Diego Ferigo <dgferigo@gmail.com>
2018-03-04 16:44:04 +0100

    Minimal pelican configuration for pelican-clean-blog theme
    Added https://github.com/getpelican/pelican-plugins/. Right now only assets is used.

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,27 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Select a pelican theme folder
+THEME = './themes/pelican-clean-blog'
+THEME_STATIC_PATHS = ['static/raw']
+
+# Disable default pages
+TAG_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+ARCHIVES_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+
+# Plugins
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['pelican-plugins.assets']
+
+# assets configuration
+ASSET_SOURCE_PATHS = [
+    'static/scss',
+    'static/css',
+    'static/js',
+]
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'https://diegoferigo.github.io/pelican-clean-blog/output'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
